fac0cfbc96ae93a2fe49951bb52642d109a46885
jhansivemula <vjhansi450@gmail.com>
2024-01-08 16:35:54 +0530

    i added in 2 classes

---
+++ src/test/java/mavenpro/Class4.java
@@ -0,0 +1,10 @@
+package mavenpro;
+
+public class Class4 {
+
+	public static void main(String[] args) {
+	System.out.println("class4");
+
+	}
+
+}
---
+++ src/test/java/mavenpro/Class5.java
@@ -0,0 +1,10 @@
+package mavenpro;
+
+public class Class5 {
+
+	public static void main(String[] args) {
+	System.out.println("class5");
+
+	}
+
+}
